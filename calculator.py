a43d15b25b6a93754c0246b2f8b7c2979a3e80bc
VextorHorizon <Coptersrichanth@gmail.com>
2025-09-03 21:30:59 +0700

    a calculator with select , Multiply , Divide

--- calculator.py
+++ calculator.py
@@ -2,17 +2,38 @@ def Plus(a,b):
     return(a + b)
 def Minus(a,b):
     return(a - b)
+def Multiply(a,b):
+    return(a * b)
+def Divide(a,b):
+    return(a / b)
 
-select = input("select 1 for Plus or select 2 for Minus : ")
+select = input(
+    "Select Operation \n"
+    "1: Plus\n"
+    "2: Minus\n"
+    "3: Multiply\n"
+    "4: Divide\n"
+    "Choose your Operation: "
+    )
 
-a = float(input("First number : "))
-b = float(input("Second number : "))
+if select in ["1","2","3","4"]:
 
-if select == "1":
-    result = Plus(a,b)
-    print(result)
-elif select == "2":
-    result = Minus(a,b)
-    print(result)
+    a = float(input("First number : "))
+    b = float(input("Second number : "))
+
+
+    if select == "1":
+        result = Plus(a,b)
+        print(result)
+    elif select == "2":
+        result = Minus(a,b)
+        print(result)
+    elif select == "3":
+        result  = Multiply(a,b)
+        print(result)
+    elif select == "4":
+        result = Divide(a,b)
+        print(result)
+
 else:
-    print("Select Fail")
+    print("Selection Failed")
